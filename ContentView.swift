bc2c6553982868f893da717d299a36832555773e
mausayao <mauricio.sayao@gmail.com>
2024-04-24 21:22:39 -0400

    config project

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIServerDriven
+//
+//  Created by Maurício Sayão on 24/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIServerDrivenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIServerDrivenApp.swift
+//  SwiftUIServerDriven
+//
+//  Created by Maurício Sayão on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIServerDrivenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
