6c3cf1d5c178e06c4f6699d0bc2acd8fc7048d35
mausayao <mauricio.sayao@gmail.com>
2024-04-29 22:03:55 -0400

    featured image component + UIComponent protocol

---
+++ Components/FeaturedImageComponent.swift
@@ -0,0 +1,29 @@
+//
+//  FeaturedImageComponent.swift
+//  SwiftUIServerDriven
+//
+//  Created by Maurício Sayão on 29/04/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct FeaturedImageComponent: UIComponent {
+    let uiModel: FeaturedImageUIModel
+    
+    var id: String {
+        ComponentType.featuredImage.rawValue
+    }
+    
+    func render() -> AnyView {
+        
+        AsyncImage(url: uiModel.imageUrl) { image in
+            image.resizable()
+                .aspectRatio(contentMode: .fill)
+        } placeholder: {
+            ProgressView()
+        }.toAnyView()
+    }
+    
+    
+}
---
+++ UIModels/FeaturedImageUIModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeaturedImageUIModel.swift
+//  SwiftUIServerDriven
+//
+//  Created by Maurício Sayão on 29/04/24.
+//
+
+import Foundation
+
+struct FeaturedImageUIModel: Decodable {
+    let imageUrl: URL
+}
